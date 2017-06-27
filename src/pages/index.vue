3c7d1333503f10ec392ec392b4c4a82a2eba2a22
chenlong <chenlongwill@qq.com>
2017-06-27 13:24:31 +0800

    完成送礼选择部分

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -160,7 +160,7 @@
                   <div class="carousel-inner">
                       <div class="item active">
                           <ol class="list-inline inner-content">
-                            <li v-for="gift in gifts">
+                            <li v-for="gift in gifts" v-bind:class="{active:gift.isSelected}" @click="selectedGift(gift)">
                               <img v-bind:src="gift.imgurl" alt="">
                               <p>{{gift.name}}</p>
                               <p><img src="../../static/images/gold.png" alt="" /><span>{{gift.price}}</span></p>
@@ -253,7 +253,12 @@ export default {
         let that = this;
         api.getGift().then(function(res){
           if(res.data.Code ==3){
-            that.gifts = res.data.Data;
+            let prices = res.data.Data;
+            for(let i in prices){
+              prices[i].isSelected = false;
+            }
+            that.gifts = prices;
+
           }
         }).catch(function(err){
           console.log(err);
@@ -263,27 +268,34 @@ export default {
     //直播
     playZhibo (){
       let that = this;
-        api.getZhibo().then(function(res){
-          if(res.data.Code ==3){
-            let url = res.data.Data[0].url;
-            let objectPlayer = new aodianPlayer({
-                container: 'player', //播放器容器ID，必要参数
-                rtmpUrl: url, //控制台开通的APP rtmp地址，必要参数
-                width: '858', //播放器宽度，可用数字、百分比等
-                height: '454', //播放器高度，可用数字、百分比等
-                autostart: true, //是否自动播放，默认为false
-                bufferlength: '3', //视频缓冲时间，默认为3秒。hls不支持！手机端不支持
-                maxbufferlength: '2', //最大视频缓冲时间，默认为2秒。hls不支持！手机端不支持
-                stretching: '1', //设置全屏模式,1代表按比例撑满至全屏,2代表铺满全屏,3代表视频原始大小,默认值为1。hls初始设置不支持，手机端不支持
-                controlbardisplay: 'enable', //是否显示控制栏，值为：disable、enable默认为disable。
-                isfullscreen: true, //是否双击全屏，默认为true
-            });
-          }else {
-                alert("无法请求到直播地址......");
-            }
-        }).catch(function(err){
-          console.log(err);
-        });
+      api.getZhibo().then(function(res){
+        if(res.data.Code ==3){
+          let url = res.data.Data[0].url;
+          let objectPlayer = new aodianPlayer({
+              container: 'player', //播放器容器ID，必要参数
+              rtmpUrl: url, //控制台开通的APP rtmp地址，必要参数
+              width: '858', //播放器宽度，可用数字、百分比等
+              height: '454', //播放器高度，可用数字、百分比等
+              autostart: true, //是否自动播放，默认为false
+              bufferlength: '3', //视频缓冲时间，默认为3秒。hls不支持！手机端不支持
+              maxbufferlength: '2', //最大视频缓冲时间，默认为2秒。hls不支持！手机端不支持
+              stretching: '1', //设置全屏模式,1代表按比例撑满至全屏,2代表铺满全屏,3代表视频原始大小,默认值为1。hls初始设置不支持，手机端不支持
+              controlbardisplay: 'enable', //是否显示控制栏，值为：disable、enable默认为disable。
+              isfullscreen: true, //是否双击全屏，默认为true
+          });
+        }else {
+              alert("无法请求到直播地址......");
+          }
+      }).catch(function(err){
+        console.log(err);
+      });
+    },
+
+    selectedGift(gift){
+      for(let i in this.gifts){
+        this.gifts[i].isSelected = false;
+      }
+      gift.isSelected = !gift.isSelected;
     }
   }
 
